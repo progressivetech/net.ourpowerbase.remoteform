312eeea2b330493c9c32678b07add709e1fab18f
Jamie McClelland <jm@mayfirst.org>
2024-07-01 17:25:02 -0400

    civix upgrade

--- remoteform.php
+++ remoteform.php
@@ -15,39 +15,12 @@ function remoteform_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function remoteform_civicrm_xmlMenu(&$files) {
-  _remoteform_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
  */
 function remoteform_civicrm_install() {
   _remoteform_civix_civicrm_install();
-}
-
-/**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function remoteform_civicrm_postInstall() {
-  _remoteform_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function remoteform_civicrm_uninstall() {
-  _remoteform_civix_civicrm_uninstall();
 }
 
 /**
@@ -59,72 +32,6 @@ function remoteform_civicrm_enable() {
   _remoteform_civix_civicrm_enable();
 }
 
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function remoteform_civicrm_disable() {
-  _remoteform_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function remoteform_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _remoteform_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function remoteform_civicrm_managed(&$entities) {
-  _remoteform_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function remoteform_civicrm_caseTypes(&$caseTypes) {
-  _remoteform_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
- */
-function remoteform_civicrm_angularModules(&$angularModules) {
-  _remoteform_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function remoteform_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _remoteform_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
 // --- Functions below this ship commented out. Uncomment as required. ---
 
 /**
@@ -132,9 +39,8 @@ function remoteform_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
  *
-function remoteform_civicrm_preProcess($formName, &$form) {
 
-} // */
+ // */
 
 /**
  * Implements hook_civicrm_navigationMenu().
@@ -162,7 +68,6 @@ function remoteform_civicrm_navigationMenu(&$menu) {
 function remoteform_civicrm_idsException(&$skip) {
   $skip[] = 'civicrm/remoteform';
 }
-
 
 /** 
  * Get displayable code.
@@ -309,7 +214,6 @@ function remoteform_civicrm_buildForm($formName, &$form) {
     remoteform_add_enable_field($form, 'profile', E::ts('Allow remote submissions to this profile.'), $code);
   }
 }
-
 
 /**
  * Save remoteform enabled settings.
